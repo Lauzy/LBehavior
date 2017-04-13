4743b0419f6eb4a46ac5ff17b2601112f7183e1f
Lauzy <freedompaladin@gmail.com>
2017-04-13 23:48:41 +0800

    debug

--- lbehaviorlib/src/main/java/com/lauzy/freedom/lbehaviorlib/behavior/LFabVerticalBehavior.java
+++ lbehaviorlib/src/main/java/com/lauzy/freedom/lbehaviorlib/behavior/LFabVerticalBehavior.java
@@ -22,20 +22,20 @@ public class LFabVerticalBehavior extends CommonBehavior {
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        if (isInit) {
+        /*if (isInit) {
             mCommonAnim = new LFabVerticalBehaviorAnim(parent, child);
             isInit = false;
-        }
+        }*/
         return super.onDependentViewChanged(parent, child, dependency);
     }
 
     //判断垂直滑动
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
-        /*if (isInit) {
+        if (isInit) {
             mCommonAnim = new LFabVerticalBehaviorAnim(coordinatorLayout, child);
             isInit = false;
-        }*/
+        }
         return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 }
